e1aa507e5038c4fffbecfb9bb6ee1ce6e0fdd755
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-11-22 19:01:31 +1030

    Added Animal class

--- animal.py
+++ animal.py
@@ -5,4 +5,29 @@ Author: Prince Gautam
 Student ID: 110351192
 Username: gaupy005
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+
+class Animal:
+    def __init__(self, name, species, age, diet_type):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.diet_type = diet_type
+        self.health_issues = []
+
+    def make_sound(self):
+        return self.name + " makes a sound"
+
+    def eat(self):
+        return self.name + " is eating " + self.diet_type + " food"
+
+    def sleep(self):
+        return self.name + " is sleeping"
+
+    def add_health_issue(self, issue):
+        self.health_issues.append(issue)
+
+    def is_healthy(self):
+        return self.health_issues == []
+
