0312be8ffbfda87f63bf2dd78ccf6fbe15ff0a12
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-11-22 21:54:47 +1030

    Implement zoo system demo in main file

--- main.py
+++ main.py
@@ -5,4 +5,36 @@ Author: Prince Gautam
 Student ID: 110351192
 Username: gaupy005
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+from animal import Animal
+from enclosure import Enclosure
+from staff import Staff
+
+print("=== Prince's Zoo Simulation ===")
+
+# create an animal
+tiger = Animal("liger", "Tiger", 8, "Meat")
+print("Animal created:", tiger.get_name())
+
+# create an enclosure
+den = Enclosure("Large", "Den")
+print("Enclosure created:", den.get_environment_type())
+
+# add animal to enclosure
+den.add_animal(tiger)
+
+# list animals in enclosure
+print("Animals in enclosure:")
+den.list_animals()
+
+# create staff
+keeper = Staff("Prince", "Zookeeper")
+print("Staff created:", keeper.get_name())
+
+# staff actions
+keeper.feed_animal(tiger)
+keeper.clean_enclosure(den)
+keeper.check_health(tiger)
+
+print("=== Demo Finished ===")
