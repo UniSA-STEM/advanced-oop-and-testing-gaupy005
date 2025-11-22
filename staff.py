972900343254a441b1d50b7c5c6beab0e67c2f5d
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-11-22 20:02:12 +1030

    Added Staff class

--- staff.py
+++ staff.py
@@ -5,4 +5,19 @@ Author: Prince Gautam
 Student ID: 110351192
 Username: gaupy005
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+
+class Staff:
+    # this creates a staff member with a name and role
+    def __init__(self, name, role):
+        self._name = name
+        self._role = role
+
+    # getter for name
+    def get_name(self):
+        return self._name
+
+    # getter for role
+    def get_role(self):
+        return self._role
