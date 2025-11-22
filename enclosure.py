a86cf061c5398d082e507b2bd4f155f6a405aa6d
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-11-22 19:23:32 +1030

    Added basic Enclosure class

--- enclosure.py
+++ enclosure.py
@@ -5,4 +5,19 @@ Author: Prince Gautam
 Student ID: 110351192
 Username: gaupy005
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
+
+class Enclosure:
+    # this creates an enclosure with a size and environment type
+    def __init__(self, size, environment_type):
+        self._size = size
+        self._environment_type = environment_type
+        self._animals = []
+
+    # getter for size
+    def get_size(self):
+        return self._size
+
+    # getter for environment type
+    def get_environment_type(self):
+        return self._environment_type
