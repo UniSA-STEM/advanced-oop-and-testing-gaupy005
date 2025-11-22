9e292de5762440e1cccbd79b097aa02571dc5340
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-11-22 20:21:43 +1030

    Added staff actions in staff class

--- staff.py
+++ staff.py
@@ -21,3 +21,18 @@ class Staff:
     # getter for role
     def get_role(self):
         return self._role
+
+    # this makes the staff member feed an animal
+    def feed_animal(self, animal):
+        print(self._name + " fed " + animal.get_name())
+
+    # this makes the staff member clean an enclosure
+    def clean_enclosure(self, enclosure):
+        print(self._name + " cleaned the enclosure.")
+
+    # this makes the vet check an animal
+    def check_health(self, animal):
+        if animal.is_healthy():
+            print(animal.get_name() + " is healthy.")
+        else:
+            print(animal.get_name() + " has health issues.")
