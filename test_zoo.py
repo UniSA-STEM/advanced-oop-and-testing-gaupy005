6783ab2b537dafbc5611ba200ade068233fb4240
gaupy005 <gaupy005@mymail.unisa.edu.au>
2025-11-24 12:27:45 +1030

    Expanded unit tests for Animal

--- test_zoo.py
+++ test_zoo.py
@@ -3,3 +3,9 @@ from animal import Animal
 def test_animal_name():
     a = Animal("Test", "TestSpecies", 2, "Food")
     assert a.get_name() == "Test"
+
+def test_animal_health():
+    a = Animal("Test", "TestSpecies", 2, "Food")
+    assert a.is_healthy() is True
+
+    
